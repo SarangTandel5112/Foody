05670e6ed582671be4088dc508f91cbbb698cc61
SarangTandel5112 <sarangtandel5112@gmail.com>
2022-06-23 10:29:16 +0530

    update cart

--- Back-end/src/controllers/cartController.ts
+++ Back-end/src/controllers/cartController.ts
@@ -3,11 +3,23 @@ import food from "../models/food";
 import user from "../models/user";
 import cartDetails from "../models/cartDetails";
 import requestInterface from "../interface/requestInterface";
-
 import cart from "../models/cart";
+import User from "../models/user";
 
 class cartController {
     // ------------ AddCart -----------------//
+
+    public fetchcart = async (req: requestInterface, res: Response) => {
+
+        const userId = req.user.id;
+        const user = await User.findById(userId);
+        if (user) {
+            const userCart = await cart.findById(user.cartId).populate({ path: "cartDetailsId" });
+            return res.status(200).json({ data: userCart });
+        }
+
+    }
+
     public addCart = async (req: requestInterface, res: Response) => {
 
         const { quantity, description } = req.body;
@@ -42,6 +54,43 @@ class cartController {
             return res.status(200).json({ data: "Item Added to cart successfully" })
         }
     }
+
+    public updateCart = async (req: requestInterface, res: Response) => {
+
+        let { cartId } = req.params;
+        cartId = cartId.trim()
+        const userId = req.user.id;
+        // console.log(cartId);
+        const cartupdate = await cartDetails.findById(cartId);
+
+        if (!cartupdate) {
+            return res.status(404).json({ data: "Item not found" })
+        }
+
+        const checkcart = await cart.findById(cartupdate.cartId);
+
+        if (checkcart) {
+            if (checkcart.userId.toString() !== userId) {
+                return res.status(400).json({ data: "Item does not exist to your account" })
+            }
+        }
+        const dataupdate = req.body;
+        const { quantity } = req.body;
+        if (quantity) {
+            const fooddata = await food.findById(cartupdate.foodId);
+            // console.log(cartupdate.quantity);
+            const sum = Number(quantity) - Number(cartupdate.quantity);
+            if (fooddata) {
+                const editprice = sum * Number(fooddata.price);
+                // console.log(editprice);
+                await cart.findByIdAndUpdate(cartupdate.cartId, { $set: { totalPrice: Number(checkcart?.totalPrice) + Number(editprice) } }, { new: true })
+                const temp = await cartDetails.findByIdAndUpdate(cartId, { $set: { ...dataupdate } }, { new: true })
+                // console.log(temp);
+            }
+        }
+        return res.status(200).json({ data: "Cart Update Successfully" })
+    }
+
 }
 
 export default cartController;

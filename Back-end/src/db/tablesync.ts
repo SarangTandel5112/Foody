a8ed1714b7c2df1d1f459c9dcf1db081bd140649
SarangTandel5112 <sarangtandel5112@gmail.com>
2022-06-24 10:25:10 +0530

    cart

--- Back-end/src/db/tablesync.ts
+++ Back-end/src/db/tablesync.ts
@@ -1,14 +1,20 @@
 import createUser from "../model_sequelize/user";
 import { Sequelize, DataType } from 'sequelize-typescript'
 import createCart from "../model_sequelize/cart";
+import createCartDetails from "../model_sequelize/cartDetails";
 
 async function tablesync(db: any) {
 
     db.users = createUser(db.sequelize, DataType)
     db.cart = createCart(db.sequelize, DataType)
+    db.cartdetails = createCartDetails(db.sequelize, DataType) 
+
+    
 
     await db.users.hasOne(db.cart, { onDelete: 'CASCADE' })
     await db.cart.belongsTo(db.users, { onDelete: 'CASCADE' })
+    await db.cart.hasMany(db.cartdetails, { onDelete: 'CASCADE' })
+    await db.cartdetails.belongsTo(db.cart, { onDelete: 'CASCADE' })
     // db.cart.belongsTo(db.users, { foreignKey: 'id', onDelete: 'cascade', hooks: true })
 
     db.sequelize.sync()
--- Back-end/src/interface/sequelizeDbInterface.ts
+++ Back-end/src/interface/sequelizeDbInterface.ts
@@ -2,7 +2,8 @@ interface sequelizeDb {
     Sequelize: any,
     sequelize: any,
     users?: any,
-    cart?: any
+    cart?: any,
+    cartdetails?: any
 }
 
 export default sequelizeDb;
